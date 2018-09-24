0e3dbb82b15a0ccd1a48a74657bafd12c438aa6d
MaurizioB <maurizio.berti@gmail.com>
2018-09-24 22:35:44 +0200

    Path fixes

--- setup_build.py
+++ setup_build.py
@@ -77,7 +77,6 @@ files = [
     'bigglesworth/presets/blofeld_fact_200802.mid', 
     'bigglesworth/presets/blofeld_fact_201200.mid', 
     'bigglesworth/help.qch', 
-    'bigglesworth/bigglesworth.qhc', 
     ]
 
 files.extend(glob('bigglesworth/ui/*.ui'))
